5bfb3a253dd8b1c803e7b31dc7e4eb5297f079b5
david <david@davids-MacBook-Pro-2.local>
2016-10-01 15:07:38 -0400

    Updating master

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  resources :dives, :users
+  devise_for :users
+  resources :dives
+
+  devise_scope :user do
+  authenticated :user do
+    root "dives#index", as: :authenticated_root
+  end
+  unauthenticated :user do
+    root "devise/sessions#create", as: :unauthenticated_root
+  end
+end
 end
